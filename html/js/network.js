040e2fa25e3d84e544b1e9f050e4af820d07c1e5
Kenley Cheung <ken@lordkenthegreat.com>
2009-06-14 09:35:26 -0400

    Added the HTML layout and supporting files

---
+++ html/js/network.js
@@ -0,0 +1,7 @@
+/*
+ * Common JavaScript for lordkenthegreat.com
+ * Available under the Creative Commons BY-NC-SA
+ */
+
+Cufon.replace('.network h1');
+Cufon.replace('.network a');
---
+++ html/js/video.js
@@ -0,0 +1,15 @@
+/*
+ * Special video player JavaScript for lordkenthegreat.com
+ * Available under the Creative Commons BY-NC-SA
+ */
+
+$f("player", "swf/flowplayer-3.1.1.swf",  {
+
+	clip: {
+		autoPlay: false
+	}
+/*	plugins: {		
+		controls: conf.skins.gray		
+	}
+*/
+});
