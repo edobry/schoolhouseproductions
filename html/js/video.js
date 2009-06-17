17053ab1f2e04cc9204c2eb86914460ff4510278
Kenley Cheung <ken@lordkenthegreat.com>
2009-06-17 16:01:33 -0400

    Refined some HTML here and there.

--- html/js/video.js
+++ html/js/video.js
@@ -7,9 +7,19 @@ $f("player", "swf/flowplayer-3.1.1.swf",  {
 
 	clip: {
 		autoPlay: false
-	}
-/*	plugins: {		
-		controls: conf.skins.gray		
-	}
-*/
+		}
+
 });
+
+// This is supposed to hide the comments, leaving an option to reveal them.  It doesn't work for some reason.
+/*
+$(document).ready(function () {
+   $("#comments").hide();
+   $("#comments-preload").click(function () {
+       $("#comments").slideDown("slow");
+       $("#comments-preload").slideUp("slow");
+   }
+   alert("test!");
+});
+
+*/
